e4bc4c85f87fe9e7c63a350045fc46e72ff6ddb9
erikamou <rm94675@estudante.fieb.edu.br>
2025-03-19 08:27:52 -0300

    component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,28 @@ function App(){
 
   const[nome, setNome] = useState('ronaldo')
 
+const Formulario = () => {
+
+  return (
+    <div>
+    
+     <input 
+    classname="nome" 
+    onChange={(e)=>{setNome(e.target.value)}}
+    name="nome"
+    type="text" />
+    <button
+    onClick={()=> {alert(nome)}}
+    classname="botao">
+    CLIQUE AQUI
+    </button>
+    </div>
+  )
+  }
+}
+const Formulario = () => {
+
+
   return (
     <div>
       <h3>pizzaria 2c</h3>
@@ -21,5 +43,14 @@ function App(){
     </div>
   )
 }
+
+  return (
+    <div>
+      <h3>pazzaria2c</h3>
+      <formulario/>
+      <formulario/>
+      <formulario/>
+    </div>
+  )
 
 export default App
